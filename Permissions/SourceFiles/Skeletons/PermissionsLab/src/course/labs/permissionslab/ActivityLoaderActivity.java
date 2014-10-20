d6ed8c39feb2e0eaf7966748a5ac3b50e34ffe4e
LiRen Tu <tuliren.git@outlook.com>
2014-10-19 18:03:10 -0700

    W03 permissions A

--- Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/ActivityLoaderActivity.java
+++ Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/ActivityLoaderActivity.java
@@ -32,7 +32,8 @@ public class ActivityLoaderActivity extends Activity {
 		
 		Log.i(TAG, "Entered startBookMarksActivity()");
 	
-		// TODO - Start the BookmarksActivity
-
+		// Start the BookmarksActivity
+    Intent bookmarksIntent = new Intent(ActivityLoaderActivity.this, BookmarksActivity.class);
+    startActivity(bookmarksIntent);
 	}
 }
--- Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
+++ Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
@@ -78,8 +78,9 @@ public class BookmarksActivity extends Activity {
 
 		Log.i(TAG, "Entered startGoToDangerousActivity()");
 
-		// TODO - Start the GoToDangerousActivity
-		
+		// Start the GoToDangerousActivity
+		Intent dangerousIntent = new Intent(BookmarksActivity.this, GoToDangerousActivity.class);
+    startActivity(dangerousIntent);
 
 	}
 
